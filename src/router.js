b6655aa8eb3170e3146573de52864c3c0273973d
陈俊宇 <1127047952@qq.com>
2019-10-08 19:43:01 +0800

    第二次提交

--- src/router.js
+++ src/router.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import registe from '/@views/registe'
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path:'/registe',
+      component:registe,
+      name:'registe'
+    }
   ]
 })
---
+++ src/views/registe.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="box">
+        <h2>注册</h2>
+        <span>用户名：</span><input type="text" placeholder="请输入爱称">
+        <span>密码：</span><input type="password" placeholder="请输入密码">
+        <button>注册</button>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="less">
+    #box{
+        height: 100%;
+        background-color: red;
+    }
+</style>
