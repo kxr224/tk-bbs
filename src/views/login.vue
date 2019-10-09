18827d5032eaabed5070f0aff686557f34a12e42
陈俊宇 <1127047952@qq.com>
2019-10-09 20:16:55 +0800

    做发送短信出现跨域问题

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,82 +1,77 @@
 <template>
-
-  <div class="main" id="id">
-    <h2>bbTalk</h2>
-    <mt-field label="用户名" placeholder="请输入用户名" v-model="username" @keyup="check"></mt-field>
-
-    <mt-field label="密码" placeholder="请输入密码" type="password" v-modal="password" @keyup="check"></mt-field>
-    <div class="but">
-      <mt-button type="primary" size="large" @click="log">登陆</mt-button>
-
-  
-        <p>没有账号？去<router-link to="/registe">注册</router-link>></p>
-       <a href="http://">忘记密码</a>
-   
-
+    <div class="main" id="id">
+    
+        <h2>bbTalk</h2>
+    
+        <mt-field label="用户名" placeholder="请输入用户名" v-model="username" @keyup="checkName()"></mt-field>
+    
+    
+    
+        <mt-field label="密码" placeholder="请输入密码" type="password" v-model="password" @keyup="checkName"></mt-field>
+    
+        <div class="but">
+    
+            <mt-button type="primary" size="large" @click="log">登陆</mt-button>
+    
+        </div>
+        <p class="noNum">
+    
+            没有账号？去
+    
+            <router-link to="/registe">注册</router-link>
+    
+        </p>
+    
+        <a href="http://">忘记密码</a>
+    
     </div>
-
-    <p class="noNum">
-      没有账号？去
-      <a href>注册</a>
-    </p>
-    <a href="http://">忘记密码</a>
-  </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      username: "",
-      password: ""
-    };
-  },
-  methods: {
-    checkName() {
-      if (this.username.length == 0) {
-        this.msgName = "用户名不能为空";
-      } else if (this.username.length < 2) {
-        this.msgName = "用户名至少2个字符";
-      } else {
-        this.msgName = "";
-      }
+    data() {
+        return {
+            username: "",
+            password: ""
+        };
     },
-    checkPassword() {
-      var word = this.password.trim();
-      if (word == "") {
-        this.msgpassword = "密码不能为空";
-      } else {
-        var count = 0;
-        if (/[0-9]/.test(word)) {
-          count++;
+    methods: {
+        checkName() {
+            if (this.username.length == 0) {
+                this.msgName = "用户名不能为空";
+            } else if (this.username.length < 2) {
+                this.msgName = "用户名至少2个字符";
+            } else {
+                this.msgName = "";
+            }
+        },
+        checkPassword() {
+            var word = this.password.trim();
+            if (word == "") {
+                this.msgpassword = "密码不能为空";
+            } else {
+            
+            }
+        },
+        log() {
+
         }
-        if (/[A-Za-z]/.test(word)) {
-          count++;
-        }
-        if (/[^0-9A-Za-z]/.test(word)) {
-          count++;
-        }
-      }
-    },
-    log(){
-        
     }
-  }
 };
 </script>
 
 <style lang="less">
 .main {
-  text-align: center;
-  margin-top: 50px;
+    text-align: center;
+    margin-top: 50px;
 }
 
 .but {
-  margin-top: 30px;
-  margin-left: 5px;
+    margin-top: 30px;
+    margin-left: 5px;
 }
 
 .noNum {
-  margin-top: 50px;
+    margin-top: 50px;
 }
 </style>
