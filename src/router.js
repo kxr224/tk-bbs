d514e9ec9e222c7e1d5bd01864adad2abb903188
陈俊宇 <1127047952@qq.com>
2019-10-09 08:59:44 +0800

    完成注册页面

--- src/router.js
+++ src/router.js
@@ -1,15 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import registe from './views/registe.vue'
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {path:'/',redirect:'/registe'},
-    {
-      path:'/registe',
-      component:registe,
-      name:'registe'
-    }
+  
   ]
 })
