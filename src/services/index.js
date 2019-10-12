3c1eafd0f22efdfd046d261b5d29ca8342d269e9
陈俊宇 <1127047952@qq.com>
2019-10-12 20:30:12 +0800

    完成了帖子详情的页面

--- src/services/index.js
+++ src/services/index.js
@@ -24,5 +24,44 @@ function getRecomList(){
 function getTheme(){
     return http.post("/api/bbs/bbsCategory/open/list")
 }
+//获取评论文本的
+function getCommentContent(postsId){
+    let formdate = new FormData()
+    formdate.append('postsId',postsId)
+    formdate.append('rememberMe',true)
+    return http.post('/api/bbs/bbsComment/open/list',formdate)
+}
+//发送评论
+function sendComment(postsId,commentContent){
+    let formdate = new FormData()
+    formdate.append('postsId',postsId)
+    formdate.append('commentContent',commentContent)
+    formdate.append('rememberMe',true)
+    return http.post('/api/bbs/bbsComment/site/add',formdate)
+}
+//发送评论的回复
+function sendRbComment(postsId,parentId,commentContent){
+    let formdate = new FormData()
+    formdate.append('postsId',postsId)
+    formdate.append('parentId',parentId)
+    formdate.append('commentContent',commentContent)
+    formdate.append('rememberMe',true)
+    return http.post('/api/bbs/bbsComment/site/reply/add',formdate)
+}
+function getRbCommentList(parentId){
+    let formdate = new FormData()
+    formdate.append('parentId',parentId)
+    formdate.append('rememberMe',true)
+    return http.post('/api/bbs/bbsComment/open/reply/list',formdate)
+}
+//删除评论接口
+function deleteComment(ids){
+    let formdate = new FormData()
+    formdate.append('ids',ids)
+    formdate.append('rememberMe',true)
+    return http.post('/api/bbs/bbsComment/site/remove',formdate)
+}
 export {getTopics,publish,iJoin,getRecomList,
-    getTheme}
+    getTheme,getCommentContent,sendComment,sendRbComment,getRbCommentList,
+    deleteComment
+}
